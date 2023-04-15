f6b4729ff624eee49a24beb5fef1a22e15147151
Antônia Turcatto <lucasturcatto.va048@academico.ifsul.edu.br>
2023-04-15 11:07:45 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+fun main() {
+    //1 - Crie um range de números de 1 a 10 e imprima cada número:
+    for(i in 1 .. 10){
+        if(i<10) {
+            print("$i,")
+        } else {
+            print("$i \n")
+        }
+    }
+
+    //2 - Crie um range de letras de 'a' a 'z' e imprima cada letra:
+    for(letra:Char in 'a'..'z'){
+        if(letra<'z'){
+        print("$letra-")
+        } else {
+            print("$letra\n")
+        }
+    }
+
+    //Crie um range de números de 1 a 100 e imprima apenas os números pares:
+    for(i in 1..100){
+        if(i%2==0){
+            if(i<100){
+                print("$i,")
+            }else{
+                print(i)
+            }
+        }
+    }
+}
